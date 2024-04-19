86dc244abd92dbdce07cef336f7cb318160bf211
xdjuniorr11 <xdjuniorr11@gmail.com>
2024-04-19 10:24:20 -0600

    add a do while condition to get out of the bucle

--- src/main/java/fordtest/testford/App.java
+++ src/main/java/fordtest/testford/App.java
@@ -2,36 +2,36 @@ package fordtest.testford;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        // declaramos la varible que tomara el numero que queremos converitr a romano
+        // we declared the number that we want to convert to roman number
         int numeroConvertir;
         Scanner entrada = new Scanner(System.in);
+        int getout;
+        // add a do while condition to get out of the bucle
+        do {
 
-        // pedimos el ingreso del numero
+        // request the number 
+
         System.out.println( "-----------------numeros naturales a romanos!-------------- " );
         System.out.println( "Ingresa el numero a convertir: " );
         numeroConvertir = entrada.nextInt();
         System.out.println( "numero "+ numeroConvertir);
 
-
-
+        convertidorRomano(numeroConvertir);
+        System.out.println( "Desea salir? presione 0  " );
+        getout =entrada.nextInt();
+        } while (getout != 0);
         
-
-
 
     }
 
     public static String convertidorRomano(int numero) {
-        int numNatural;
+        
         int indiceMil,indiceCen,indiceDec,indiceUn;
-        /// U D C PARA LA CONVERSION DE LOS NUMEROS 
+        /// U D C to be able to convert the roman numbers
 
         String[]  uni = {"","I","II","III","IV","V","VI","VII","VII","IX"}; //DECLARAMOS EL ARREGLO DE LAS CENTENAS
         String[]  dec = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};//DECLARAMOS EL ARREGLO DE LAS DECENAS
@@ -49,11 +49,9 @@ public class App
         indiceDec= numero /10;
         numero = numero%10;
         indiceUn = numero;
-
-
+        
+        System.out.println( mil[indiceMil] + cen[indiceCen] + dec[indiceDec] + uni[indiceUn]);
         return mil[indiceMil] + cen[indiceCen] + dec[indiceDec] + uni[indiceUn];
-
-
 
     }
 }
