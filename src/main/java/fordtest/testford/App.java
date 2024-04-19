2c86b340257d835519ecb9519b061a23a4b09739
xdjuniorr11 <xdjuniorr11@gmail.com>
2024-04-19 09:10:16 -0600

    integracion numero natural

--- src/main/java/fordtest/testford/App.java
+++ src/main/java/fordtest/testford/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "numeros naturales a romanos! " );
+
+    }
+
+    public static void convertidorRomano(int numero) {
+        int numNatural;
+        
     }
 }
