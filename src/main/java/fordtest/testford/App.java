9fd961fac194cbc914360832ce58136986ae30b4
xdjuniorr11 <xdjuniorr11@gmail.com>
2024-04-19 10:05:16 -0600

    convertidorRomanmapeara los numeros romanos deacuerdo a numero que se  ingrese

--- src/main/java/fordtest/testford/App.java
+++ src/main/java/fordtest/testford/App.java
@@ -28,14 +28,32 @@ public class App
 
     }
 
-    public static void convertidorRomano(int numero) {
+    public static String convertidorRomano(int numero) {
         int numNatural;
-
+        int indiceMil,indiceCen,indiceDec,indiceUn;
         /// U D C PARA LA CONVERSION DE LOS NUMEROS 
 
-        String[]  uni = {"I","II","III","IV","V","VI","VII","VII","IX"}; //DECLARAMOS EL ARREGLO DE LAS CENTENAS
-        String[]  dec = {"X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};//DECLARAMOS EL ARREGLO DE LAS DECENAS
-        String[]  cen = {"C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};//DECLARAMOS EL ARREGLO DE LAS CENTENAS
+        String[]  uni = {"","I","II","III","IV","V","VI","VII","VII","IX"}; //DECLARAMOS EL ARREGLO DE LAS CENTENAS
+        String[]  dec = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};//DECLARAMOS EL ARREGLO DE LAS DECENAS
+        String[]  cen = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};//DECLARAMOS EL ARREGLO DE LAS CENTENAS
+        String[]  mil = {"","M","MM","MMM"};
+
+        // we need to see de residuo to be able to map with the index, that is going to help us to indetfy which number 
+        // we are going to remplace
+        indiceMil= numero /1000;
+        numero = numero%1000;
+
+        indiceCen= numero /100;
+        numero = numero%100;
+
+        indiceDec= numero /10;
+        numero = numero%10;
+        indiceUn = numero;
+
+
+        return mil[indiceMil] + cen[indiceCen] + dec[indiceDec] + uni[indiceUn];
+
+
 
     }
 }
