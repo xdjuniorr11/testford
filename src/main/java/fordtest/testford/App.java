2fd5d41aee2b3d778500f43c6dd6562b1f7fb91a
xdjuniorr11 <xdjuniorr11@gmail.com>
2024-04-19 09:04:31 -0600

    inicio del desarrollo

--- src/main/java/fordtest/testford/App.java
+++ src/main/java/fordtest/testford/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "numeros naturales a romanos!" );
+        System.out.println( "numeros naturales a romanos! " );
     }
 }
