6a034dddd456b8e27f03b2794356b8aa5308bce5
xdjuniorr11 <xdjuniorr11@gmail.com>
2024-04-19 09:42:01 -0600

    /// U D C PARA LA CONVERSION DE LOS NUMEROS

--- src/main/java/fordtest/testford/App.java
+++ src/main/java/fordtest/testford/App.java
@@ -14,6 +14,7 @@ public class App
         int numeroConvertir;
         Scanner entrada = new Scanner(System.in);
 
+        // pedimos el ingreso del numero
         System.out.println( "-----------------numeros naturales a romanos!-------------- " );
         System.out.println( "Ingresa el numero a convertir: " );
         numeroConvertir = entrada.nextInt();
@@ -29,6 +30,12 @@ public class App
 
     public static void convertidorRomano(int numero) {
         int numNatural;
-        /// test commit
+
+        /// U D C PARA LA CONVERSION DE LOS NUMEROS 
+
+        String[]  uni = {"I","II","III","IV","V","VI","VII","VII","IX"}; //DECLARAMOS EL ARREGLO DE LAS CENTENAS
+        String[]  dec = {"X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};//DECLARAMOS EL ARREGLO DE LAS DECENAS
+        String[]  cen = {"C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};//DECLARAMOS EL ARREGLO DE LAS CENTENAS
+
     }
 }
