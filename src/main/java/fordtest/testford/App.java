4355341d63bc8704adfddc17c83afbc2a0976b42
Jose Miguel <xdjuniorr11@gmail.com>
2024-04-26 20:12:58 -0600

    // we need to see de residuo to be able to map with the index, that is going to help us to indetfy which number         // we are going to remplace

--- src/main/java/fordtest/testford/App.java
+++ src/main/java/fordtest/testford/App.java
@@ -1,20 +1,17 @@
 package fordtest.testford;
-
 import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        // we declared the number that we want to convert to roman number
+        // we declared the number that we want to convert to Roman number
         int numeroConvertir;
         Scanner entrada = new Scanner(System.in);
         int getout;
-        // add a do while condition to get out of the bucle
+        // add a do while condition to get out of the buckle
         do {
-
         // request the number 
-
         System.out.println( "-----------------numeros naturales a romanos!-------------- " );
         System.out.println( "Ingresa el numero a convertir: " );
         numeroConvertir = entrada.nextInt();
@@ -23,23 +20,22 @@ public class App
         convertidorRomano(numeroConvertir);
         System.out.println( "Desea salir? presione 0  " );
         getout =entrada.nextInt();
-        } while (getout != 0);
-        
+        } while (getout != 0);  
 
     }
 
     public static String convertidorRomano(int numero) {
         
         int indiceMil,indiceCen,indiceDec,indiceUn;
-        /// U D C to be able to convert the roman numbers
+        /// U D C to be able to convert the Roman numbers
 
         String[]  uni = {"","I","II","III","IV","V","VI","VII","VII","IX"}; //DECLARAMOS EL ARREGLO DE LAS CENTENAS
         String[]  dec = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};//DECLARAMOS EL ARREGLO DE LAS DECENAS
         String[]  cen = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};//DECLARAMOS EL ARREGLO DE LAS CENTENAS
         String[]  mil = {"","M","MM","MMM"};
 
-        // we need to see de residuo to be able to map with the index, that is going to help us to indetfy which number 
-        // we are going to remplace
+        // we need to see the residue to be able to map with the index, that is going to help us to which number 
+        // we are going to replace
         indiceMil= numero /1000;
         numero = numero%1000;
 
